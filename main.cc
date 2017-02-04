e95aaed38770eafbc6a0c4e49e7073aaec5203a3
David Bergström <david-bergstrom@outlook.com>
2017-02-05 00:03:28 +0100

    Add a game update listener

--- main.cc
+++ main.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <sio_client.h>
 
 #include "map.h"
@@ -6,6 +8,7 @@
 network n;
 
 void on_connected();
+void on_game_update(sio::event& event);
 
 int main() {
     n.connect([]() { std::cout << "Connected" << std::endl; });
@@ -15,4 +18,8 @@ void on_connected() {
     n.login();
     n.join_private("arstarstarst");
     n.set_force_start("arstarstarst", true);
+}
+
+void on_game_update(sio::event& event) {
+    std::cout << event.get_name() << std::endl;
 }
