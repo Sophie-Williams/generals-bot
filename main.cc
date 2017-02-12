b3a9d38d7415d3d400c0c164b416f161319116b3
David Bergström <david-bergstrom@outlook.com>
2017-02-12 17:07:35 +0100

    Fix connection error (Server handshake response error)
    Resolves #1

--- main.cc
+++ main.cc
@@ -5,15 +5,16 @@
 #include "map.h"
 #include "network.h"
 
-network n;
 
 void on_connected();
 void on_game_update(sio::event& event);
 
 int main() {
+    network n;
     n.connect([]() { std::cout << "Connected" << std::endl; });
 }
 
+/*
 void on_connected() {
     n.login();
     n.join_private("arstarstarst");
@@ -23,3 +24,4 @@ void on_connected() {
 void on_game_update(sio::event& event) {
     std::cout << event.get_name() << std::endl;
 }
+*/
